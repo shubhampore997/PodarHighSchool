c0219f8c2b4cc5e80debd64f9f37727d8eb88e1a
shubham Ravikiran Pore <shubhampore997@gmail.com>
2024-04-30 02:46:19 -0500

    Podar Highschool

---
+++ Javascript/First.js
@@ -0,0 +1,40 @@
+// console.log("Welcome To Js");
+// console.log('Hi Im Shubham Ravikiran Pore');
+// age = 23
+// console.log(age);
+// name="piyush"
+// console.log(name);
+// x=null;
+// console.log(x);
+// y=undefined;
+// console.log(y);
+// name=25;
+// console.log(name);
+
+
+// name='shubham'
+// Name='Pore'
+// console.log(name)
+// console.log(Name)
+
+
+
+let name='pore'
+let age=24;
+
+var a=24
+var a=67
+var a=98
+console.log(a)
+
+let b =89;
+
+
+
+const a1 =34;
+a1=98
+let v;
+
+console.log(a1)
+console.log(age)
+console.log(name)
